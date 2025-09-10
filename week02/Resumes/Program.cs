ecb5fefcfa42abb81845f42d79e7a3b0dc758419
Mystic Muffin <mm.devlogs@gmail.com>
2025-09-09 18:24:05 -0600

    completed week 2 learning activity abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Adobe";
+        job2._jobTitle = "Graphic Designer";
+        job2._startYear = 2000;
+        job2._endYear = 3000;
+
+        Resume myResume = new Resume();
+        myResume._personName = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
